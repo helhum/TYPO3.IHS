5d5c5f276c137281bdc6a94229576307cc53f2d6
Helmut Hummel <helmut.hummel@typo3.org>
2014-06-01 23:28:51 +0200

    Add sub routes to package

--- Classes/TYPO3/IHS/Controller/StandardController.php
+++ Classes/TYPO3/IHS/Controller/StandardController.php
@@ -13,10 +13,8 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	/**
 	 * @return void
 	 */
-	public function indexAction() {
-		$this->view->assign('foos', array(
-			'bar', 'baz'
-		));
+	public function redirectAction() {
+		$this->redirect('index', 'Advisory');
 	}
 
 }
