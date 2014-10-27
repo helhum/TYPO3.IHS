1dd0ce9101f3ec1742600fb13d79ce0605bc3923
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2014-10-27 13:15:30 +0100

    Removed relocation to intercepted request on login
    * because it is not needed anymore and prevents the bug with the redirect to /login/login

--- Packages/Application/TYPO3.IHS/Classes/TYPO3/IHS/Controller/LoginController.php
+++ Packages/Application/TYPO3.IHS/Classes/TYPO3/IHS/Controller/LoginController.php
@@ -49,16 +49,12 @@ class LoginController extends AbstractAuthenticationController {
 	}
 
 	/**
-	 * Redirects to a potentially intercepted request. Returns an error message if there has been none.
+	 * Redirects to Advisory Controller after login
 	 *
 	 * @param \TYPO3\Flow\Mvc\ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
 	 * @return string
 	 */
 	protected function onAuthenticationSuccess(\TYPO3\Flow\Mvc\ActionRequest $originalRequest = NULL) {
-		if ($originalRequest !== NULL) {
-			$this->redirectToRequest($originalRequest);
-		}
-
-		$this->redirect('Index', 'Issue');
+		$this->redirect('Index', 'Advisory', 'TYPO3.IHS');
 	}
 }
